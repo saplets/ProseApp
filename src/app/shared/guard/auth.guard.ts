552b555f4d34447b4ea19ba6900c5a997f001b4a
unknown <sapletsmail@gmail.com>
2019-08-24 15:40:18 -0700

    Simplified the whole beast down.

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -7,11 +7,14 @@ export class AuthGuard implements CanActivate {
     constructor(private router: Router) {}
 
     canActivate() {
-        if (localStorage.getItem('isLoggedin')) {
-            return true;
-        }
+        return true;
 
-        this.router.navigate(['/login']);
-        return false;
+        // TODO: Eventually we'll check for the session, but not now
+        // if (localStorage.getItem('isLoggedin')) {
+        //     return true;
+        // }
+
+        // this.router.navigate(['/login']);
+        // return false;
     }
 }
